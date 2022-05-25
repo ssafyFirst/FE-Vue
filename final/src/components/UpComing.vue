92b8e12a35bdd8c846f7d690f16795266be306b4
민경욱 <rrkcl7733@naver.com>
2022-05-25 19:21:28 +0900

    d

--- final/src/components/UpComing.vue
+++ final/src/components/UpComing.vue
@@ -1,42 +1,21 @@
 <template>
-<<<<<<< HEAD
-  <span>
   <b-carousel
-    id="carousel-1"
+    id="carousel"
     v-model="slide"
-    :interval="3000"
+    :interval="4000"
     controls
     indicators
     img-width="400"
-    img-height="320"
+    img-height="480"
   >
+
     <up-coming-item
-    v-for="( movie, index ) in upcomingMovie"
+    v-for="( movie, index) in upcomingMovie"
     :key="movie.id"
     :movie="movie"
     :index="index"
     ></up-coming-item>
-=======
-  <div>
-    <b-carousel
-      id="carousel"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      img-width="400"
-      img-height="480"
-    >
-
-      <up-coming-item
-      v-for="( movie, index) in upcomingMovie"
-      :key="movie.id"
-      :movie="movie"
-      :index="index"
-      ></up-coming-item>
->>>>>>> 2458e1f8f5e474da821d53e50df635f4f18ca7f2
   </b-carousel>
-  </span>
 </template>
 
 <script>
