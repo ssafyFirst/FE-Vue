eda69801b8deebb558bfeb0bb4d82ebc6d396657
YoonKyu <newjeff@naver.com>
2022-05-27 01:04:46 +0900

    recommend

---
+++ final/src/components/RecommendMovieList.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="recommendmovie">
+  
+    <b-card-group columns>
+      <recommend-movie-item v-for="movie in recommendMovies"
+      :key="movie.id"
+      :movie="movie"
+      style="float: left;"
+      ></recommend-movie-item>
+    </b-card-group>
+  
+</div>
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import RecommendMovieItem from '@/components/RecommendMovieItem.vue'
+
+export default {
+  name:'RecommendMovieList',
+  components: { RecommendMovieItem },
+  computed:{
+    ...mapGetters(['recommendMovies'])
+  }
+}
+</script>
+
+<style>
+#recommendmovie {
+  width: 80%;
+  margin: auto;
+}
+</style>
